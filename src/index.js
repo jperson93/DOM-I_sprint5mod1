81433e520f9087894e1373f54c15c21d6c3e09ca
Jb Person <homeaccount@Jbs-MacBook-Air.local>
2023-02-28 22:19:38 -0500

    almost done

--- src/index.js
+++ src/index.js
@@ -39,4 +39,48 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+//IMAGES
+const logoImg = document.querySelector('#logo-img');
+logoImg.src = siteContent.images['logo-img'];
+
+const ctaImg = document.querySelector('#cta-img'); 
+ctaImg.src = siteContent.images['cta-img'];
+
+const middleImg = document.querySelector('#middle-img'); 
+middleImg.src = siteContent.images['accent-img']; 
+
+
+//TEXT CONTENT 
+
+//footerlink 
+const footerLink = document.querySelector('footer a'); 
+footerLink.textContent = siteContent.footer.copyright; 
+footerLink.classList.add('bold'); 
+
+//contact section 
+const contact = document.querySelector('section.contact'); 
+contact.children[0].textContent = siteContent.contact['contact-h4']
+//  OR 
+// contact.querySelector('h4').textContent = siteContent.contact['contact-h4'];
+
+contact.children[1].textContent = siteContent.contact['address']; 
+contact.children[2].textContent = siteContent.contact['phone']; 
+contact.children[3].textContent = siteContent.contact['email']; 
+
+//TOP CONTENT 
+const topContent = document.querySelector('.top-content'); 
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content'];
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
+
+
+// BOTTOM CONTENT
+const bottomContent = document.querySelector('.bottom-content'); 
+console.log(bottomContent); 
+bottomContent.children[0].children[0].textContent = siteContent['main-content']['services-h4']; 
+bottomContent.children[0].children[1].textContent = siteContent['main-content']['services-content']; 
+bottomContent.children[1].children[0].textContent = siteContent['main-content']['product-h4']; 
+bottomContent.children[1].children[1].textContent = siteContent['main-content']['product-content']; 
+bottomContent.children[2].children[0].textContent = siteContent['main-content']['vision-h4']; 
+bottomContent.children[2].children[1].textContent = siteContent['main-content']['vision-content']; 
